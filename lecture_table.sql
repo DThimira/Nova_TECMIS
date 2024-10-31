5bf66ca373b7b72e5b560442ca7023b0fc89e71b
Dilusha1347 <dildevi9@gmail.com>
2024-10-31 23:27:22 +0000

    insert_data_lecture

--- lecture_table.sql
+++ lecture_table.sql
@@ -11,3 +11,21 @@ CREATE TABLE lecture (
     Age INT
 );
 
+
+
+
+
+
+
+INSERT INTO lecture (Lecture_id, F_name, L_name, Department, Address, Email, Position, Gender, Age) VALUES
+('L001', 'Amila', 'Wickramarathna', 'ET', '123 Galle Road, Colombo', 'amila.wikr@example.com', 'Senior Lecturer', 'Male', 45),
+('L002', 'Navodi', 'Somathilaka', 'BST', '456 Bandaranaike Mawatha, Kandy', 'navodi.soma@example.com', 'Assistant Professor', 'Female', 38),
+('L003', 'Ranil', 'Kelaniya', 'IT', '789 Mahaweli Street, Nuwara Eliya', 'ranil.kelaniya@example.com', 'Professor', 'Male', 50),
+('L004', 'Charitha', 'Devapriya', 'ET', '321 Ratnapura Road, Galle', 'charitha.deva@example.com', 'Lecturer', 'Female', 34),
+('L005', 'Dinusha', 'Jayasinghe', 'BST', '654 Ratmalana Road, Colombo', 'dinusha.jaya@example.com', 'Senior Lecturer', 'Female', 40),
+('L006', 'Aruna', 'Peiris', 'IT', '987 Western Highway, Negombo', 'aruna.peiris@example.com', 'Associate Professor', 'Male', 47),
+('L007', 'Shihan', 'Samarawira', 'ET', '159 Parakramabahu Mawatha, Kalutara', 'shihan.sam@example.com', 'Lecturer', 'Male', 30),
+('L008', 'Sachini', 'Welgama', 'BST', '753 Kelaniya Road, Colombo', 'sachini.welgama@example.com', 'Professor', 'Female', 55),
+('L009', 'Indika', 'Pathiraja', 'IT', '951 Kurunegala Road, Kurunegala', 'indika.pati@example.com', 'Assistant Lecturer', 'Male', 33),
+('L010', 'Lahiru', 'Hevawitharana', 'ET', '852 Waskaduwa Road, Moratuwa', 'lahiru.hewa@example.com', 'Lecturer', 'Male', 42),
+('L011', 'Malith', 'Kumarasinghe', 'BST', '753 Pannipitiya Road, Maharagama', 'malith.kuma@example.com', 'Senior Lecturer', 'Male', 41);
