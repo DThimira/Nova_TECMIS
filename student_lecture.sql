69d42ff021ce76dd63a41eb8f188e62614305267
Dilusha1347 <dildevi9@gmail.com>
2024-10-31 22:48:49 +0000

    student_lecture

--- student_lecture.sql
+++ student_lecture.sql
@@ -1,11 +1,10 @@
-
-CREATE TABLE student_lecture
-(
-
-Reg_no VARCHAR (20),
-Lecture_id VARCHAR(20),
-FOREIGN KEY(Lecture_id) REFERENCES lecture (Lecture_id) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY(Reg_no) REFERENCES student (Reg_no) ON DELETE CASCADE ON UPDATE CASCADE
-
-
+CREATE TABLE student_lecture (
+    Reg_no VARCHAR(20),
+    Lecture_id VARCHAR(20),
+    FOREIGN KEY (Lecture_id) REFERENCES lecture (Lecture_id) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (Reg_no) REFERENCES student (Reg_no) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
 );
