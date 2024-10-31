fece3f0cfda67606bbbe2c79b804f857d2e90753
Dilusha1347 <dildevi9@gmail.com>
2024-10-31 23:30:25 +0000

    insert_data_student_lecture

--- student_lecture.sql
+++ student_lecture.sql
@@ -8,3 +8,21 @@ CREATE TABLE student_lecture (
         ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
+
+
+
+
+
+INSERT INTO student_lecture (Reg_no, Lecture_id) VALUES
+('TG/2022/1001', 'L001'),
+('TG/2022/1002', 'L002'),
+('TG/2022/1003', 'L003'),
+('TG/2022/1004', 'L004'),
+('TG/2022/1005', 'L005'),
+('TG/2022/1006', 'L006'),
+('TG/2022/1007', 'L007'),
+('TG/2022/1008', 'L008'),
+('TG/2022/1009', 'L009'),
+('TG/2022/1010', 'L010'),
+('TG/2022/1011', 'L011');
+
