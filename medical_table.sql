a416d8b352f67f9ca564cd6aca717b312877f3ff
Dilusha1347 <dildevi9@gmail.com>
2024-11-03 14:48:04 +0000

    medical_table

--- medical_table.sql
+++ medical_table.sql
@@ -4,26 +4,25 @@ CREATE TABLE medical (
     End_date DATE,
     Description VARCHAR(100),
     Reg_no VARCHAR(20),
-    Tech_id VARCHAR(20),
+    Tech_id INT,
     FOREIGN KEY (Reg_no) REFERENCES student(Reg_no) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE,
-    FOREIGN KEY (Tech_id) REFERENCES teach_officer(Tech_id) 
+    FOREIGN KEY (Tech_id) REFERENCES tec_officer(Tech_id) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
 
 
 
-
-INSERT INTO medical (Medical_Id, Start_date, End_date, Description, Reg_no, Tech_id) VALUES
-('M001', '2023-01-05', '2023-01-10', 'Flu and fever', 'TG/2022/1001', 'TO001'),
-('M002', '2023-02-15', '2023-02-20', 'Fractured arm', 'TG/2022/1002', 'TO002'),
-('M003', '2023-03-01', '2023-03-07', 'Chickenpox', 'TG/2022/1003', 'TO003'),
-('M004', '2023-04-10', '2023-04-14', 'Migraine', 'TG/2022/1004', 'TO004'),
-('M005', '2023-05-20', '2023-05-25', 'Allergic reaction', 'TG/2022/1005', 'TO005'),
-('M006', '2023-06-01', '2023-06-05', 'Dengue fever', 'TG/2022/1006', 'TO006'),
-('M007', '2023-07-15', '2023-07-18', 'Severe cold', 'TG/2022/1007', 'TO007'),
-('M008', '2023-08-05', '2023-08-10', 'Back pain', 'TG/2022/1008', 'TO008'),
-('M009', '2023-09-10', '2023-09-12', 'Food poisoning', 'TG/2022/1009', 'TO009'),
-('M010', '2023-10-15', '2023-10-20', 'Typhoid fever', 'TG/2022/1010', 'TO010');
+INSERT INTO medical (Medical_id, Start_date, End_date, Description, Reg_no, Tech_id) VALUES
+('M001', '2024-09-05', '2024-09-10', 'Flu and fever', 'TG/2022/001', '104'),
+('M002', '2024-08-15', '2024-08-20', 'Fractured arm', 'TG/2022/002', '100'),
+('M003', '2024-10-01', '2024-10-07', 'Chickenpox', 'TG/2020/002', '102'),
+('M004', '2024-09-10', '2024-09-13', 'Migraine', 'TG/2022/004', '103'),
+('M005', '2024-11-01', '2024-11-05', 'Allergic reaction', 'TG/2022/005', '105'),
+('M006', '2024-09-23', '2024-10-04', 'Dengue fever', 'TG/2022/004', '100'),
+('M007', '2024-09-16', '2024-09-18', 'Severe cold', 'TG/2022/002', '101'),
+('M008', '2024-08-23', '2024-08-28', 'Back pain', 'TG/2022/008', '102'),
+('M009', '2024-09-10', '2024-09-12', 'Food poisoning', 'TG/2021/001', '105'),
+('M010', '2024-10-15', '2024-10-18', 'Typhoid fever', 'TG/2022/010', '104');
