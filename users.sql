56353ac0c0cb7ea78c5f1bdce987a7f6e60967a6
W.I.D.Wijesinghe <w.i.dhananjaya80@gmail.com>
2024-11-05 02:22:41 +0530

    users_updated

--- users.sql
+++ users.sql
@@ -1,39 +1,38 @@
-CREATE DATABASE NovaTecmis;
+CREATE DATABASE Nova_Tecmis;
 USE NovaTecmis;
 
 
 CREATE USER 'Admin'@'localhost' IDENTIFIED BY 'admin1234';
-GRANT ALL PRIVILEGES ON NovaTecmis.* TO 'Admin'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON Nova_Tecmis.* TO 'Admin'@'localhost' WITH GRANT OPTION;
 
 
 CREATE USER 'Dean'@'localhost' IDENTIFIED BY 'dean1234';
-GRANT ALL PRIVILEGES ON NovaTecmis.* TO 'Dean'@'localhost';
+GRANT ALL PRIVILEGES ON Nova_Tecmis.* TO 'Dean'@'localhost';
 
 
 CREATE USER 'Lecturer'@'localhost' IDENTIFIED BY 'lecture1234';
-GRANT SELECT, INSERT, UPDATE, DELETE ON NovaTecmis.* TO 'Lecturer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON Nova_Tecmis.* TO 'Lecturer'@'localhost';
 
 
-CREATE USER 'TechnicalOfficer'@'localhost' IDENTIFIED BY 'tech1234';
-GRANT SELECT, INSERT, UPDATE ON NovaTecmis.attendance TO 'TechnicalOfficer'@'localhost';
+CREATE USER 'tec_officer'@'localhost' IDENTIFIED BY 'tec1234';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.ATTENDANCE TO 'tec_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.attendance_by_session_type TO 'tec_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.attendance_summary   TO 'tec_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.attendance_individual  TO 'tec_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.attendance_summary_by_course  TO 'tec_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Nova_Tecmis.attendance_with_medical  TO 'tec_officer'@'localhost';
 
 
 CREATE USER 'Student'@'localhost' IDENTIFIED BY 'stu1234';
-GRANT SELECT ON NovaTecmis.attendance_summary TO 'Student'@'localhost';
-GRANT SELECT ON NovaTecmis.Student_Grades TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.attendance_summary TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.Student_Grades TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.CA_Marks_Details TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.CA_Eligibility_Status TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.End_Marks TO 'Student'@'localhost';
+GRANT SELECT ON Nova_Tecmis.Final_Marks_Details TO 'Student'@'localhost';
 
 
 FLUSH PRIVILEGES;
-
-
-
----------------------------------------
-Passwords
-A$dM!n_2024@# for Admin
-D#eAn_2024$! for Dean
-L#eC_tURe!_2024 for Lecturer
-T@ChNi_C0FF!2024 for Technical Officer
-Stu#D3nT@2024! for Student
 
 
 SELECT User, Host FROM mysql.user;
