f471f638f7e0d59e5c1097c4ae589efb7692ea55
Dilusha1347 <dildevi9@gmail.com>
2024-11-05 10:36:24 +0000

    cgpa_view

--- cgpa.sql
+++ cgpa.sql
@@ -1,13 +1,21 @@
-CREATE VIEW Student_CGPA AS
-SELECT 
-    s.Reg_no,
-    AVG(sg.SGPA) AS CGPA
-FROM 
-    Student_SGPA sg
-JOIN 
-    student s ON sg.Reg_no = s.Reg_no
-GROUP BY 
-    s.Reg_no;
+
+
+CREATE VIEW CGPA AS
+SELECT
+    student_ID,
+    SUM(Semester_GPA * Total_Credits) / NULLIF(SUM(Total_Credits), 0) AS CGPA
+FROM (
+    SELECT
+        sg.student_ID,
+        sg.Semester_GPA,
+        SUM(c.Credit) AS Total_Credits
+    FROM Semester_GPA sg
+    JOIN Final_Marks_Details final ON sg.student_ID = final.Reg_no
+    JOIN course c ON final.Course_Code = c.Course_code
+    WHERE c.Course_code != 'ENG1222'  
+    GROUP BY sg.student_ID, sg.Semester_GPA
+) AS Semester_Calculations
+GROUP BY student_ID;  
 
 
 
