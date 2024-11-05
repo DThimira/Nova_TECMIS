e4225be19088c76b51017a78243547075c8f5b7f
Dilusha1347 <dildevi9@gmail.com>
2024-11-05 11:37:34 +0000

    cgpa_view

--- cgpa.sql
+++ cgpa.sql
@@ -2,20 +2,19 @@
 
 CREATE VIEW CGPA AS
 SELECT
-    student_ID,
+    reg_no,
     SUM(Semester_GPA * Total_Credits) / NULLIF(SUM(Total_Credits), 0) AS CGPA
 FROM (
     SELECT
-        sg.student_ID,
+        sg.reg_no,
         sg.Semester_GPA,
         SUM(c.Credit) AS Total_Credits
     FROM Semester_GPA sg
-    JOIN Final_Marks_Details final ON sg.student_ID = final.Reg_no
+    JOIN Final_Marks_Details final ON sg.reg_no = final.Reg_no
     JOIN course c ON final.Course_Code = c.Course_code
-    WHERE c.Course_code != 'ENG1222'  
-    GROUP BY sg.student_ID, sg.Semester_GPA
+    WHERE c.Course_code != 'ENG1222'  -- Exclude the ENG1222 course
+    GROUP BY sg.reg_no, sg.Semester_GPA
 ) AS Semester_Calculations
-GROUP BY student_ID;  
-
+GROUP BY reg_no;  -- Group by student_ID to calculate CGPA for each student
 
 
